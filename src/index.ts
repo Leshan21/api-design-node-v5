d072ed608826462a845b6b7481b566d986e4514e
Leshan <leshanpasindu12@gmail.com>
2025-11-27 08:34:47 +0530

    create sample server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/health', (req, res) => { // req is what the client sends, res is what we send back from the server
+  res.json({ message: 'API is healthy' }).status(200); // Corrected order of method calls
+})
+
+export { app }; // Export the app for use in other modules
+
+export default app; // Default export for convenience means we can import without curly braces
