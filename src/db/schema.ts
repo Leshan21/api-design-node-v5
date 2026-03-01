c4946568c29e1b6c732f91d2cb3d215ae118912b
Leshan <leshanpasindu12@gmail.com>
2026-03-01 09:46:55 +0530

    add habit_tags table and update schema for user, habits, entries, and tags

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,65 @@
 import {
-    pgTable,
-    uuid,
-    text,
-    varchar,
-    timestamp,
-    boolean,
-    integer,
-} from 'drizzle-orm/pg-core';
-import { relations } from 'drizzle-orm';
+  pgTable,
+  uuid,
+  text,
+  varchar,
+  timestamp,
+  boolean,
+  integer,
+} from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { createDeflate } from 'zlib'
 
 export const user = pgTable('user', {
-    id: uuid('id').primaryKey().defaultRandom(),
-    email: varchar('email', { length: 255 }).notNull().unique(),
-    
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  username: varchar('username', { length: 50 }).notNull().unique(),
+  password: varchar('password', { length: 255 }),
+
+  firstName: varchar('first_name', { length: 50 }),
+  lastName: varchar('last_name', { length: 50 }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updateAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const habits = pgTable('habits', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'cascade' }), // foreign key to user table
+  name: varchar('name', { length: 100 }).notNull(),
+  description: text('description'),
+  frequency: varchar('frequency', { length: 20 }).notNull(),
+  targetCount: integer('target_count').default(1),
+  isActive: boolean('is_active').default(true).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updateAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const entries = pgTable('entries', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitsId: uuid('habits_id')
+    .notNull()
+    .references(() => habits.id, { onDelete: 'cascade' }),
+  completionDate: timestamp('completion_date').defaultNow().notNull(),
+  note: text('note'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
+
+export const tags = pgTable('tags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', { length: 50 }).notNull().unique(),
+  color: varchar('color', { length: 7 }).default('#6b7280'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updateAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const habitTags = pgTable('habit_tags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitsId: uuid('habits_id')
+    .notNull()
+    .references(() => habits.id, { onDelete: 'cascade' }),
+  tagId: uuid('tag_id')
+    .notNull()
+    .references(() => tags.id, { onDelete: 'cascade' }),
 })
