1861bdf84d80e9ad53765f7726c77266771803b9
Leshan <leshanpasindu12@gmail.com>
2026-03-02 08:32:57 +0530

    add JWT utility; implement JWT generation with payload structure and signing

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import { SignJWT } from 'jose'
+import env from '../../env.ts'
+import { createSecretKey } from 'crypto'
+
+export interface JWTPayload {
+  // Define the shape of your JWT payload
+  id: string
+  username: string
+  email: string
+}
+
+export const generateJWT = async (payload: JWTPayload) => {
+  const secret = env.JWT_SECRET
+  const secretKey = createSecretKey(secret, 'utf-8')
+
+  return await new SignJWT(payload)
+  .setProtectedHeader({ alg: 'HS256' })
+  .setIssuedAt()
+  .setExpirationTime(env.JWT_EXPIRES_IN || '7d')
+  .sign(secretKey)
+}
