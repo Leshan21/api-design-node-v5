a9cdf3b1d8c2527810cd917c4e786c8f84df46bd
Leshan <leshanpasindu12@gmail.com>
2026-03-02 10:46:20 +0530

    refactor registration route; implement validation middleware and connect to registration controller

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'; // Import Router from express
+import { register } from '../controllers/authController.ts'; // Import the register function from authController
+import { validateBody } from '../middleware/validation.ts';
+import { insertUserSchema } from '../db/schema.ts';
 
 const router = Router(); // Create a new router instance
 
 // Define a POST route for user registration
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'User registered successfully' });
-})
+router.post('/register', validateBody(insertUserSchema) ,register)
 
 // Define a POST route for user login
 router.post('/login', (req, res) => {
